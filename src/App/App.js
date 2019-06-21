d208ecdd586c4d7a3a2fc70dbb3eb49aa2eef043
Jon Crenshaw <jdcrensh@gmail.com>
2019-06-21 16:13:09 -0700

    chore: implement prettier

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import * as S from './styles';
 
-function App() {
-  return (
-    <>
-      <S.App>
-        <S.Header>
-          <S.Logo />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <S.LearnReact
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </S.LearnReact>
-        </S.Header>
-      </S.App>
-    </>
-  );
-}
+const App = () => (
+  <>
+    <S.App>
+      <S.Header>
+        <S.Logo />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <S.LearnReact
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </S.LearnReact>
+      </S.Header>
+    </S.App>
+  </>
+);
 
 export default App;
